03179db8a2b3845e0a4894a33ccef49f3403ad08
G/ <g@moigneu.com>
2026-07-26 11:33:42 +0200

    fix(web): isolate task views from brief state

---
+++ server/internal/web/ui/assets/t._taskId-B6YHdqWB.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/t._taskId-DcQZUGhR.js","assets/dist-CaejBv-z.js","assets/checkmate-app-X-Cyoa4H.js","assets/api-Bf9SFSKI.js","assets/dist-G8gGUkCh.js"])))=>i.map(i=>d[i]);
+import{n as e,r as t,t as n}from"./preload-helper-C7g3hz7K.js";var r=t(`/t/$taskId`)({component:e(()=>n(()=>import(`./t._taskId-DcQZUGhR.js`),__vite__mapDeps([0,1,2,3,4])),`component`)});export{r as t};
---
+++ server/internal/web/ui/assets/t._taskId-DcQZUGhR.js
@@ -0,0 +1,1 @@
+import{U as e}from"./dist-CaejBv-z.js";import{t}from"./checkmate-app-X-Cyoa4H.js";import{t as n}from"./t._taskId-B6YHdqWB.js";var r=e();function i(){let{taskId:e}=n.useParams();return(0,r.jsx)(t,{detailId:e})}export{i as component};
